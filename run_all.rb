2a46ca5bc30314447db57bcb2667b5f448dd1605
Assaf Shomer <assafshomer@gmail.com>
2016-01-18 16:51:37 +0200

    fixed run all

--- run_all.rb
+++ run_all.rb
@@ -1,16 +1,27 @@
 dirname = "widgets"
 
-dont_run_me = [
+only_run_me = [
+	'main_stats',
+	'tx_last_24h',
+	'tx_since_launch_by_month',
 	'tx_last_week',
-	'piwik_engine_send_last_week'
-	].map{|n| "#{dirname}/#{n}.rb"}
-# only_run_me = ['main_stats','balance_stats'].map{|n| "#{dirname}/#{n}.rb"}
-only_run_me = [].map{|n| "#{dirname}/#{n}.rb"}
+	'balance_stats',
+	'machine_stats',
+	'piwik_transmit_last_week',
+	'piwik_mobile_send_last_week',
+	'piwik_build_finance_last_week'
+	]
 
 
-widgets = Dir["#{dirname}/**/*.rb"]
-widgets.reject!{|w| dont_run_me.include?(w)}
-widgets.select!{|w| only_run_me.include?(w)} unless only_run_me.empty?
+all_widgets = Dir["#{dirname}/**/*.rb"]
+
+widgets = only_run_me.map do |desired_widget|
+	all_widgets.select{|widget| widget =~ /#{desired_widget}/}
+end.flatten
+
+# all_widgets.each{|w| p w}
+# p "*"*20
+# widgets.each{|w| p w}
 
 widgets.each do |widget|
 	p "#{widget.split('/').last.split('.').first} starting"
