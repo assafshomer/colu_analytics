4caf7c70eb6123458d01b9a50914def852f30607
Assaf Shomer <assafshomer@gmail.com>
2016-01-19 15:17:23 +0200

    more cleanup

--- old/line_graph.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-curl -i -X POST -k -d '{"accessKey": "Q2OK7mbk9vGNJY5zL4OApTswYFWwqXUk", "streamName": "lL2Z2Wk8", "point": [{"number": 155, "timestamp": 1452609476515},{"number": 33, "timestamp": 1452609470000},{"number": 142, "timestamp": 1452609000000}]}' https://www.leftronic.com/customSend/
