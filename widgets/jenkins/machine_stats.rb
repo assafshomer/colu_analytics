34dd846a59fe2fe3e4534a1dc16f93c36ffcd14e
Assaf Shomer <assafshomer@gmail.com>
2016-01-21 15:20:42 +0200

    oops, another fix

--- widgets/jenkins/machine_stats.rb
+++ widgets/jenkins/machine_stats.rb
@@ -6,21 +6,29 @@ include JenkinsHelper
 mainnet_request = last_build_url('Test-GetData-MainNet')
 testnet_request = last_build_url('Test-GetData-TestNet')
 
+p "Getting Mainnet Machine Status from Jenkins"
+init_time = Time.now
 mainnet_status = JENKINS.api_get_request(mainnet_request)['result']
+p "JENKINS API replied within [#{time_diff(init_time)}]"
+
+p "Getting Testnet Machine Status from Jenkins"
+init_time = Time.now
 testnet_status = JENKINS.api_get_request(testnet_request)['result']
+p "JENKINS API replied within [#{time_diff(init_time)}]"
 
-return unless mainnet_status && testnet_status
+if (mainnet_status && testnet_status)
+	p pretty_mainnet_status = mainnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
+	p pretty_testnet_status = testnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
 
-p pretty_mainnet_status = mainnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
-p pretty_testnet_status = testnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
+	stream = 'JLZKlvnA'
+	UPDATE.clear(stream)
 
-stream = 'JLZKlvnA'
-UPDATE.clear(stream)
+	header_row = ["Network", "Status"]
+	table_rows = [
+		["Mainnet", pretty_mainnet_status],
+		["Testnet", pretty_testnet_status]
+	]
 
-header_row = ["Network", "Status"]
-table_rows = [
-	["Mainnet", pretty_mainnet_status],
-	["Testnet", pretty_testnet_status]
-]
+	UPDATE.push_table stream, header_row, table_rows	
+end
 
-UPDATE.push_table stream, header_row, table_rows
