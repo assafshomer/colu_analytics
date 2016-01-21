23314823302b46d50b5d5670ff02aa3059ad0363
Assaf Shomer <assafshomer@gmail.com>
2016-01-21 12:15:06 +0200

    better tests for date helper

--- helpers/date_helper.rb
+++ helpers/date_helper.rb
@@ -8,17 +8,21 @@ module DateHelper
 		start_time = raw_start_time.to_i * 1000
 		raw_end_time = Time.parse(end_day)+3600*24
 		end_time = raw_end_time.to_i * 1000
+		# p Time.at(start_time/1000)
+		# p Time.at(end_time/1000)
 		return {from: start_time, till: end_time}		
 	end
 
 	def days_are_numbers(limit=0,offset=0)
-		# returns the integer time reps spanning 00:00 start day to midnight end day
+		# returns the integer time reps spanning 00:00 start day to midnight end day. be default talking about today. If limit > 0 start day is limit days before start day. If offset > 0 end day is offset days before.
 		end_day = Time.at(Time.now.to_i - 3600*24*offset).strftime("%d/%m/%Y")
 		start_day = Time.at(Time.now.to_i - 3600*24*(offset+limit)).strftime("%d/%m/%Y")
 		raw_end_time = Time.parse(end_day)+3600*24
 		raw_start_time = Time.parse(start_day)
 		end_time = raw_end_time.to_i * 1000
-		start_time = raw_start_time.to_i * 1000		
+		start_time = raw_start_time.to_i * 1000
+		p Time.at(start_time/1000)
+		p Time.at(end_time/1000)		
 		return {from: start_time, till: end_time}		
 	end	
 end
--- spec/date_helper_spec.rb
+++ spec/date_helper_spec.rb
@@ -13,10 +13,24 @@ describe "DateHelper" do
 	  end
 	end
 	describe 'days_are_numbers' do
-	  it 'should return the integer time representations' do
-	  	x = days_are_numbers
+		it 'by default should span current 24h' do
+			x = days_are_numbers
 			(x[:till] - x[:from]).should == 1000*3600*24
-	  end	  
+			Time.at(x[:from]/1000).strftime("%d/%m/%Y").should == Time.now.strftime("%d/%m/%Y")
+			Time.at(x[:till]/1000).strftime("%d/%m/%Y").should == (Time.now + 3600*24).strftime("%d/%m/%Y")			
+		end
+		it 'with limit should span limit days in past untill tonight full days' do
+			x = days_are_numbers(1)
+			(x[:till] - x[:from]).should == 1000*3600*24*2
+			Time.at(x[:from]/1000).strftime("%d/%m/%Y").should == (Time.now - 3600*24).strftime("%d/%m/%Y")
+			Time.at(x[:till]/1000).strftime("%d/%m/%Y").should == (Time.now + 3600*24).strftime("%d/%m/%Y")
+		end
+		it 'with limit and offset should span limit days in past untill midnight of offset days in the past' do
+			x = days_are_numbers(1,1)
+			(x[:till] - x[:from]).should == 1000*3600*24*2
+			Time.at(x[:from]/1000).strftime("%d/%m/%Y").should == (Time.now - 3600*24*2).strftime("%d/%m/%Y")
+			Time.at(x[:till]/1000).strftime("%d/%m/%Y").should == (Time.now).strftime("%d/%m/%Y")
+		end		
 	end
 	# in web console
 	# new Date(1452722400000)
