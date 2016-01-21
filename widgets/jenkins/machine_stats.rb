637ef11a4ba0243d08b7f960503e50f5c5bcf056
Assaf Shomer <assafshomer@gmail.com>
2016-01-21 15:07:42 +0200

    trying to fix machine stats bug

--- widgets/jenkins/machine_stats.rb
+++ widgets/jenkins/machine_stats.rb
@@ -6,11 +6,13 @@ include JenkinsHelper
 mainnet_request = last_build_url('Test-GetData-MainNet')
 testnet_request = last_build_url('Test-GetData-TestNet')
 
-p mainnet_status = JENKINS.api_get_request(mainnet_request)['result']
-p testnet_status = JENKINS.api_get_request(testnet_request)['result']
+mainnet_status = JENKINS.api_get_request(mainnet_request)['result']
+testnet_status = JENKINS.api_get_request(testnet_request)['result']
+
+return unless mainnet_status && testnet_status
 
 p pretty_mainnet_status = mainnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
-p pretty_testnet_status = mainnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
+p pretty_testnet_status = testnet_status == 'SUCCESS' ? "UP (#{Time.now.strftime("%H:%M")})" : "DOWN (#{Time.now.strftime("%H:%M")})"
 
 stream = 'JLZKlvnA'
 UPDATE.clear(stream)
