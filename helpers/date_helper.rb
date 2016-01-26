be751b34718617aaa0b7bebe98322a2db51f1e76
Assaf Shomer <assafshomer@gmail.com>
2016-01-26 10:11:27 +0200

    another attempt

--- helpers/date_helper.rb
+++ helpers/date_helper.rb
@@ -5,9 +5,9 @@ module DateHelper
 	def dates_are_numbers(start_day,end_day = nil)
 		# expecting start day in format '14/01/2016', returns the integer time reps spanning 00:00 start day to midnight end day
 		end_day = herenow.strftime("%d/%m/%Y") unless end_day
-		raw_start_time = Time.parse(start_day)
+		raw_start_time = Time.parse(start_day).in_time_zone('Jerusalem')
 		start_time = raw_start_time.to_i * 1000
-		raw_end_time = Time.parse(end_day)+3600*24
+		raw_end_time = Time.parse(end_day).in_time_zone('Jerusalem')+3600*24
 		end_time = raw_end_time.to_i * 1000
 		# p Time.at(start_time/1000)
 		# p Time.at(end_time/1000)
@@ -18,8 +18,8 @@ module DateHelper
 		# returns the integer time reps spanning 00:00 start day to midnight end day. be default talking about today. If limit > 0 start day is limit days before start day. If offset > 0 end day is offset days before.
 		end_day = Time.at(herenow.to_i - 3600*24*offset).strftime("%d/%m/%Y")
 		start_day = Time.at(herenow.to_i - 3600*24*(offset+limit)).strftime("%d/%m/%Y")
-		raw_end_time = Time.parse(end_day)+3600*24
-		raw_start_time = Time.parse(start_day)
+		raw_end_time = Time.parse(end_day).in_time_zone('Jerusalem')+3600*24
+		raw_start_time = Time.parse(start_day).in_time_zone('Jerusalem')
 		end_time = raw_end_time.to_i * 1000
 		start_time = raw_start_time.to_i * 1000
 		p Time.at(start_time/1000)
