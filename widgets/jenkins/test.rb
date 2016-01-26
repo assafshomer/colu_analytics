a4290dbadb3b345557cf2c635b81fc6c47060845
Assaf Shomer <assafshomer@gmail.com>
2016-01-26 11:04:58 +0200

    and more

--- widgets/jenkins/test.rb
+++ widgets/jenkins/test.rb
@@ -4,6 +4,9 @@ include DateHelper
 
 dan = days_are_numbers
 
-p "from: #{Time.at(dan[:from]/1000)}, till: #{Time.at(dan[:till]/1000)}"
+# p "from: #{Time.at(dan[:from]/1000)}, till: #{Time.at(dan[:till]/1000)}"
 
-p "herenow :#{herenow}, #{Time.at(herenow)}"
+# p "herenow :#{herenow}, #{Time.at(herenow)}"
+
+p "here"+(Time.now.strftime("%z").to_i/100).to_s
+p "Jer" +(Time.now.in_time_zone('Jerusalem').strftime("%z").to_i/100).to_s
