a9e70f6bdee51f5d45e46fbe35a1395f108a1645
Assaf Shomer <assafshomer@gmail.com>
2016-01-26 10:02:45 +0200

    trying to fix timezone issue

--- helpers/date_helper.rb
+++ helpers/date_helper.rb
@@ -1,9 +1,10 @@
 require 'time'
+require 'active_support/time'
 
 module DateHelper
 	def dates_are_numbers(start_day,end_day = nil)
 		# expecting start day in format '14/01/2016', returns the integer time reps spanning 00:00 start day to midnight end day
-		end_day = Time.now.strftime("%d/%m/%Y") unless end_day
+		end_day = herenow.strftime("%d/%m/%Y") unless end_day
 		raw_start_time = Time.parse(start_day)
 		start_time = raw_start_time.to_i * 1000
 		raw_end_time = Time.parse(end_day)+3600*24
@@ -15,8 +16,8 @@ module DateHelper
 
 	def days_are_numbers(limit=0,offset=0)
 		# returns the integer time reps spanning 00:00 start day to midnight end day. be default talking about today. If limit > 0 start day is limit days before start day. If offset > 0 end day is offset days before.
-		end_day = Time.at(Time.now.to_i - 3600*24*offset).strftime("%d/%m/%Y")
-		start_day = Time.at(Time.now.to_i - 3600*24*(offset+limit)).strftime("%d/%m/%Y")
+		end_day = Time.at(herenow.to_i - 3600*24*offset).strftime("%d/%m/%Y")
+		start_day = Time.at(herenow.to_i - 3600*24*(offset+limit)).strftime("%d/%m/%Y")
 		raw_end_time = Time.parse(end_day)+3600*24
 		raw_start_time = Time.parse(start_day)
 		end_time = raw_end_time.to_i * 1000
@@ -35,5 +36,9 @@ module DateHelper
 		array_of_hashes_with_time_key.group_by do |element|
 			Time.at(element[:time]/1000).strftime("%d/%m/%Y %H:00")
 		end
-	end	
+	end
+
+	def herenow
+		Time.now.in_time_zone('Jerusalem')
+	end
 end
