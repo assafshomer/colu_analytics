642cba5fbe2da0a12342568ca47f8dd7c9a069a3
Assaf Shomer <assafshomer@gmail.com>
2016-01-26 11:41:53 +0200

    I think I got the sign wrong

--- helpers/date_helper.rb
+++ helpers/date_helper.rb
@@ -18,8 +18,8 @@ module DateHelper
 		# returns the integer time reps spanning 00:00 start day to midnight end day. be default talking about today. If limit > 0 start day is limit days before start day. If offset > 0 end day is offset days before.
 		end_day = Time.at(Time.now.to_i - 3600*24*offset).strftime("%d/%m/%Y")
 		start_day = Time.at(Time.now.to_i - 3600*24*(offset+limit)).strftime("%d/%m/%Y")
-		raw_end_time = Time.parse(end_day)+3600*(24 + hour_offset_to_israel) 
-		raw_start_time = Time.parse(start_day) + 3600*hour_offset_to_israel
+		raw_end_time = Time.parse(end_day)+3600*(24 - hour_offset_to_israel) 
+		raw_start_time = Time.parse(start_day) - 3600*hour_offset_to_israel
 		end_time = raw_end_time.to_i * 1000
 		start_time = raw_start_time.to_i * 1000
 		p Time.at(start_time/1000)
