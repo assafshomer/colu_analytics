ee6c6261d492de2b6299851a59d5ea18c9fbcdd0
Assaf Shomer <assafshomer@gmail.com>
2016-01-28 10:02:30 +0200

    nicer css for asset leaderboard

--- helpers/explorer_helper.rb
+++ helpers/explorer_helper.rb
@@ -115,7 +115,7 @@ module ExplorerHelper
 		html_start = '<!DOCTYPE html><html><head><title></title></head><body>'
 		html_end = '</body></html>'
 		result = html_start
-		result << %Q(<div class="widgetTitle lt-widget-title" style="left: 20.65px; font-size: 20.65px; line-height: 59px;color:rgb(204, 204, 204);"><h1>Leading Assets since midnight <div style="float:right;font-size:14px;">(updated: #{timestamp})</div></h1></div>)
+		result << %Q(<div class="widgetTitle lt-widget-title" style="left: 20.65px; font-size: 23px; line-height: 59px;color:rgb(204, 204, 204);"><h1>Leading Assets since midnight <div style="float:right;font-size:14px;">(updated: #{timestamp})</div></h1></div>)
 		ordered_asset_ids.each do |data_point|
 			asset_id = data_point.keys.first
 			short_asset_id = asset_id[0..9]+'...'
@@ -127,7 +127,7 @@ module ExplorerHelper
 			p "name: #{asset_name}, issuer: #{issuer_name}, desc: #{asset_desc}"
 			frequency = data_point[asset_id]
 			title = %Q(#{asset_name} issued by #{issuer_name} #{asset_desc})
-			line = %Q(<p><div style="line-height:35px; height:50px; font-size:30px;"><a href="http://coloredcoins.org/explorer/asset/#{asset_id}" target="_blank" style="color:rgb(0, 189, 255); right:20.65px; text-decoration:none;float:left;">#{asset_name}</a> <div style="color:rgb(204, 204, 204);float:right;text-align:right;">#{frequency}</div></div></p>)
+			line = %Q(<p><div style="line-height:35px; height:50px; font-size:30px;border-top-style: solid;clear: both;border-top-width: 1px;border-top-color:#4d4d4d;"><a href="http://coloredcoins.org/explorer/asset/#{asset_id}" target="_blank" style="color:rgb(0, 189, 255); right:20.65px; text-decoration:none;float:left;">#{asset_name}</a> <div style="color:rgb(204, 204, 204);float:right;text-align:right;">#{frequency}</div></div></p>)
 			result << line
 		end
 		result << html_end
