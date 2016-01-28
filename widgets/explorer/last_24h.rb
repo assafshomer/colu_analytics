79f508055d7db4702c4a0b0ad3d07bfdf0811674
Assaf Shomer <assafshomer@gmail.com>
2016-01-28 10:14:57 +0200

    more asset leaderboard styling

--- widgets/explorer/last_24h.rb
+++ widgets/explorer/last_24h.rb
@@ -7,6 +7,7 @@ issuance_stream = 'wbCblg7t'
 transfer_stream = 'AIceGGMQ'
 assets_stream = 'aEu4sfdm'
 
+number_of_assets = 7
 number_of_days = 1
 start_days_past = 0
 debug = false
@@ -45,7 +46,7 @@ UPDATE.push_line(transfer_stream,parsed_transfer_data)
 # point = {"leaderboard": asset_data.first(5) }
 # UPDATE.push_line(assets_stream,point)
 
-ordered_asset_ids = order_asset_ids(raw_data).first(8)
+ordered_asset_ids = order_asset_ids(raw_data).first(number_of_assets)
 html = prepare_asset_leaderboard(ordered_asset_ids)
 # UPDATE.clear(assets_stream)
 UPDATE.push_html assets_stream, html
