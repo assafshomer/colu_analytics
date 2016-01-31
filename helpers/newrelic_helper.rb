78c1bda6a35a0c547a2b8036d55ecab14a4aa901
Assaf Shomer <assafshomer@gmail.com>
2016-01-31 11:16:41 +0200

    factoring out of newrelic logic in mobile stats

--- helpers/newrelic_helper.rb
+++ helpers/newrelic_helper.rb
@@ -5,22 +5,34 @@ module NewrelicHelper
 	NEWRELIC_BASE = 'https://api.newrelic.com/v2/mobile_applications'
 	NEWRELIC_HEADER = { "X-Api-Key"=> APP_CONFIG['newrelic_api_key'] }
 
-	def newrelic_mobile_active_users
-		newrelic_url = NEWRELIC_BASE + '.json'		
-		response = HTTParty.get(newrelic_url,headers: NEWRELIC_HEADER).parsed_response			
+	def newrelic_mobile_active_users(opts={})
+		debug = opts[:debug] || false
+		newrelic_url = NEWRELIC_BASE + '.json'
+		call_newrelic_api(newrelic_url,debug)
 	end
 
 	def newrelic_mobile_data(app_id,metric,opts={})
 		from = opts[:from] || datestamp
 		to = opts[:to] || datestamp(+1)
 		period = opts[:period] || (Time.parse(to) - Time.parse(from))
+		debug = opts[:debug] || false
 		newrelic_url = NEWRELIC_BASE
 		newrelic_url += "/#{app_id}/metrics/data.json"
 		newrelic_url += "?names[]=#{metric}"
 		newrelic_url += "&from=#{from}" if from
 		newrelic_url += "&to=#{to}" if to
 		newrelic_url += "&period=#{period}" if period
-		response = HTTParty.get(newrelic_url,headers: NEWRELIC_HEADER).parsed_response
+		call_newrelic_api(newrelic_url,debug)
+	end
+
+	def call_newrelic_api(url,debug=false)
+		init_time = Time.now
+		p "Calling Newrelic API with [#{url}]" if debug
+		data = HTTParty.get(url,headers: NEWRELIC_HEADER)
+		p "Explorer API replied [#{time_diff(init_time)}]" if debug
+		response = data.parsed_response
+		p "newrelic response: #{response}" if debug
+		return response
 	end
  
 
--- widgets/newrelic/mobile_stats.rb
+++ widgets/newrelic/mobile_stats.rb
@@ -4,70 +4,60 @@ include NewrelicHelper
 include ViewsHelper
 
 stream = '3UN8UsIx'
+debug = true
 
-stat_data = newrelic_mobile_active_users['applications']
+stat_data = newrelic_mobile_active_users(debug: false)['applications']
+oss = [:android,:ios]
 
-android_id = APP_CONFIG['newrelic_android_app_id']
-ios_id = APP_CONFIG['newrelic_ios_app_id']
+ids = oss.map{|os| [os.to_sym,APP_CONFIG["newrelic_#{os}_app_id"]]}.to_h
 
-android_active_users = stat_data.select do |os|
-	os["id"] == android_id
-end.first['mobile_summary']['active_users']
-
-ios_active_users = stat_data.select do |os|
-	os["id"] == ios_id
-end.first['mobile_summary']['active_users']
+active_users = ids.map do |k,v|
+	[k,stat_data.select do |d|
+		d["id"] == v
+	end.first['mobile_summary']['active_users']]
+end.to_h
 
 android_prefix = 'Custom/androidCategory/'
 ios_prefix = 'Custom/completeSendProcessProfiling/'
+android_params = ['send to address','send to phone','sign trans.','sync contacts']
+ios_params = ['sentByAddress[seconds]','sentByPhone[seconds]','signProcess[seconds]']
 
-params = 	
-	{ios: [
-		'sentByAddress[seconds]','sentByPhone[seconds]','signProcess[seconds]'
-		], 
-	android: [
-		'send to address','send to phone','sign trans.','sync contacts'
-	]}
-
-newrelic_mobile_data(android_id,android_prefix+'send+to+address')
-
-UPDATE.clear(stream)
+prefixes = oss.map{|os| [os.to_sym,eval("#{os}_prefix")]}.to_h
+params = oss.map{|os| [os.to_sym,eval("#{os}_params")]}.to_h
 
 metrics = ["Address", "Phone","Sync"]
-
 titles = ["Send2A", "Send2P","Sync Contacts"]
-
 header_row = ["Type", "# Users"] + titles
-android = ["Android", android_active_users]
-ios = ["iOS", ios_active_users]
 
-metrics.each do |metric|
-	android_metric_name = params[:android].select{|m| m =~ /#{metric}/i}.first
-	if android_metric_name
-		android_metric_raw_data = newrelic_mobile_data(android_id,android_prefix+android_metric_name)
-		android_metric_values = android_metric_raw_data['metric_data']['metrics'].first['timeslices'].first['values']
-		p "#{android_metric_name}: #{android_metric_raw_data}"
-		android_metric_avg_value = android_metric_values['average_value']
-		android << android_metric_avg_value.round(1).to_s+' sec'
-	else
-		android << '--'
-	end
+rows = {ios:[], android: []}
+rows[:android] = ["Android", active_users[:android]]
+rows[:ios] = ["iOS", active_users[:ios]]
 
-	ios_metric_name = params[:ios].select{|m| m =~ /#{metric}/i}.first
-	if ios_metric_name
-		ios_metric_avg_value = newrelic_mobile_data(ios_id,ios_prefix+ios_metric_name)['metric_data']['metrics'].first['timeslices'].first['values']['average_value'].round(1).to_s+' sec'
-		p "#{ios_metric_name}: #{ios_metric_avg_value}"
-		ios << ios_metric_avg_value			
-	else
-		ios << '--'
-	end
-
+oss.each do |os|
+	metrics.each do |metric|
+		metric_name = params[os].select{|m| m =~ /#{metric}/i}.first
+		if metric_name			
+			metric_raw_data = newrelic_mobile_data(ids[os],prefixes[os]+metric_name,debug: false)
+			if metric_raw_data.keys.first == 'error'
+				p "#{os} | #{metric_name}: --" if debug
+				rows[os] << '--'	
+			else
+				metric_values = metric_raw_data['metric_data']['metrics'].first['timeslices'].first['values']
+				metric_avg_value = metric_values['average_value']
+				display = metric_avg_value.round(1).to_s+' sec'
+				p "#{os} | #{metric_name}: #{display}" if debug
+				rows[os] << display
+			end
+		else
+			p "#{os} | #{metric_name}: --" if debug
+			rows[os] << '--'
+		end
+	end	
 end
 
-table_rows = [android,ios]
+table_rows = oss.map{|os| rows[os]}
 
-p table_rows
-
+UPDATE.clear(stream)
 UPDATE.push_table stream, header_row, table_rows
 
 
