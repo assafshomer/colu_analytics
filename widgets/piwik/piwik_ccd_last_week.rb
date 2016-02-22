0f31cb18e7af93a97eb92a5d49b7bd660f15f7d7
Assaf Shomer <assafshomer@gmail.com>
2016-02-22 19:41:51 +0200

    added missing testnet ccd api

--- widgets/piwik/piwik_ccd_last_week.rb
+++ widgets/piwik/piwik_ccd_last_week.rb
@@ -3,20 +3,28 @@ require __dir__+'/../../setup'
 require __dir__+'/../../helpers/piwik_helper'
 include PiwikHelper
 
-stream = 'deyodFog'
+stream = {mainnet: 'deyodFog', testnet: '7SU2lAWK'}
+id_site = {mainnet: 16, testnet: 15}
+timeout = {mainnet: 120, testnet: 180}
 number_of_days = 7
 
-result = []
+[:mainnet, :testnet].each do |network|
+	# next if network == :mainnet
+	print_box("Processing #{network}")
+	begin
+	  Timeout::timeout(timeout[network]) do
 
-number_of_days.times do |n|	
-	curdate = Time.at(Time.now.to_i - 3600*24*n)
-	method = "Actions.getPageUrls"
-	segment = "pageUrl%3D%3Dhttps%253A%252F%252Fapi.coloredcoins.org%252Fv3%252Fissue,pageUrl%3D%3Dhttps%253A%252F%252Fapi.coloredcoins.org%252Fv3%252Fsendasset"
-	id_site = 16
-	raw = piwik_data_during_day(curdate,segment: segment, method: method,id_site: id_site, debug: false)
-	p raw
-	result << count_hits(raw,curdate)
+			result = []
+			number_of_days.times do |n|	
+				curdate = Time.at(Time.now.to_i - 3600*24*n)
+				nw = (network==:testnet) ? 'testnet.' : ''
+				method = "Actions.getPageUrls"
+				segment = "pageUrl%3D%3Dhttps%253A%252F%252F#{nw}api.coloredcoins.org%252Fv3%252Fissue,pageUrl%3D%3Dhttps%253A%252F%252Fapi.coloredcoins.org%252Fv3%252Fsendasset"
+				raw = piwik_data_during_day(curdate,segment: segment, method: method,id_site: id_site[network],network: network, debug: false)
+				result << count_hits(raw,curdate)
+			end			
+			UPDATE.clear(stream[network])
+			UPDATE.push_line(stream[network],result)
+		end
+	end
 end
-
-UPDATE.clear(stream)
-UPDATE.push_line(stream,result)
