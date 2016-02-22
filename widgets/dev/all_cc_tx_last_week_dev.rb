9107b4b6e5501f96e678fd6fa4530cfe6ac8b1d2
Assaf Shomer <assafshomer@gmail.com>
2016-02-22 18:07:48 +0200

    less debugging

--- widgets/dev/all_cc_tx_last_week_dev.rb
+++ widgets/dev/all_cc_tx_last_week_dev.rb
@@ -5,22 +5,24 @@ include ExplorerHelper
 
 mainnet_stream = '43d2f62784'
 
-week_ago = Time.at(Time.now.to_i - 3600*24*7).strftime("%d/%m/%Y")
+num_days = 7
 
 timeout = 60
-begin
-  Timeout::timeout(timeout) do
-		network = :mainnet
-		stream = mainnet_stream
-		data = number_of_cc_tx_by_dates(start_day: week_ago, network: network)
-		parsed_data = data.map do |x|	
-			hash = {"number" => x["txsSum"], "timestamp" => x["from"]/1000}
-			JSON.parse(hash.to_json)
-		end
-		UPDATE.clear(stream)
-		UPDATE.push_line(stream,parsed_data)  		
-  end
-rescue Timeout::Error
-	p "Explorer call timed out after #{timeout} seconds"
-end	
+[:mainnet].each do |network|
+	begin
+	  Timeout::timeout(timeout) do
+			stream = eval("#{network}_stream")
+			data = number_of_cc_tx_by_days(num_days: num_days, network: network)
+			print_box(data,'cc tx data')
+			parsed_data = data.map do |x|	
+				hash = {"number" => x["txsSum"], "timestamp" => x["from"]/1000}
+				JSON.parse(hash.to_json)
+			end
+			UPDATE.clear(stream)
+			UPDATE.push_line(stream,parsed_data)  		
+	  end
+	rescue Timeout::Error
+		p "#{network} Explorer call timed out after #{timeout} seconds"
+	end		
+end
 
--- widgets/explorer/tx_last_week.rb
+++ widgets/explorer/tx_last_week.rb
@@ -14,7 +14,6 @@ timeout = 60
 	  Timeout::timeout(timeout) do
 			stream = eval("#{network}_stream")
 			data = number_of_cc_tx_by_days(num_days: num_days, network: network)
-			print_box(data,'cc tx data')
 			parsed_data = data.map do |x|	
 				hash = {"number" => x["txsSum"], "timestamp" => x["from"]/1000}
 				JSON.parse(hash.to_json)
