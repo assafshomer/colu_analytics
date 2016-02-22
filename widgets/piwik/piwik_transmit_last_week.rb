a0361a2b818530bde4cdcae767a808dd9b2e7848
Assaf Shomer <assafshomer@gmail.com>
2016-02-22 19:17:36 +0200

    fixed missing weekly financed in testnet

--- widgets/piwik/piwik_transmit_last_week.rb
+++ widgets/piwik/piwik_transmit_last_week.rb
@@ -3,18 +3,27 @@ require __dir__+'/../../setup'
 require __dir__+'/../../helpers/piwik_helper'
 include PiwikHelper
 
-stream = 'AESSls3U'
+stream = {mainent: 'AESSls3U', testnet: '479cf0445d'}
+id_site = {mainnet: 7, testnet: 6}
+timeout = {mainnet: 120, testnet: 180}
 number_of_days = 7
 
-result = []
+[:mainnet, :testnet].each do |network|
+	# next if network == :mainnet
+	print_box("Processing #{network}")
+	begin
+	  Timeout::timeout(timeout[network]) do
 
-number_of_days.times do |n|	
-	curdate = Time.at(Time.now.to_i - 3600*24*n)
-	method = "Actions.getPageUrls"
-	segment = "pageTitle%3D%3Dengine.transmit_financed"
-	raw = piwik_data_during_day(curdate,segment: segment, method: method, debug: false)
-	result << count_hits(raw,curdate)
+			result = []
+			number_of_days.times do |n|	
+				curdate = Time.at(Time.now.to_i - 3600*24*n)
+				method = "Actions.getPageUrls"
+				segment = "pageTitle%3D%3Dengine.transmit_financed"	
+				raw = piwik_data_during_day(curdate,segment: segment, method: method,id_site: id_site[network], debug: false)
+				result << count_hits(raw,curdate)
+			end			
+			UPDATE.clear(stream[network])
+			UPDATE.push_line(stream[network],result)
+		end
+	end
 end
-
-UPDATE.clear(stream)
-UPDATE.push_line(stream,result)
