bbe0cd7db952ca5dd25c67a819eff25658ef0ee3
Assaf Shomer <assafshomer@gmail.com>
2016-02-22 19:46:12 +0200

    Added weekly mobile on testnet

--- widgets/piwik/piwik_mobile_send_last_week.rb
+++ widgets/piwik/piwik_mobile_send_last_week.rb
@@ -3,18 +3,27 @@ require __dir__+'/../../setup'
 require __dir__+'/../../helpers/piwik_helper'
 include PiwikHelper
 
-stream = 'nB1a42B9'
+stream = {mainnet: 'nB1a42B9', testnet: '5yI6V0ZQ'}
+id_site = {mainnet: 7, testnet: 6}
+timeout = {mainnet: 120, testnet: 180}
 number_of_days = 7
 
-result = []
+[:mainnet, :testnet].each do |network|
+	# next if network == :mainnet
+	print_box("Processing #{network}")
+	begin
+	  Timeout::timeout(timeout[network]) do
 
-number_of_days.times do |n|	
-	curdate = Time.at(Time.now.to_i - 3600*24*n)
-	method = "Actions.getPageUrls"
-	segment = "pageTitle%3D%3Dmobile_server.send_asset"
-	raw = piwik_data_during_day(curdate,segment: segment, method: method, debug: false)
-	result << count_hits(raw,curdate)
+			result = []
+			number_of_days.times do |n|	
+				curdate = Time.at(Time.now.to_i - 3600*24*n)
+				method = "Actions.getPageUrls"
+				segment = "pageTitle%3D%3Dmobile_server.send_asset"
+				raw = piwik_data_during_day(curdate,segment: segment, method: method,id_site: id_site[network],network: network, debug: false)
+				result << count_hits(raw,curdate)
+			end			
+			UPDATE.clear(stream[network])
+			UPDATE.push_line(stream[network],result)
+		end
+	end
 end
-
-# UPDATE.clear(stream)
-UPDATE.push_line(stream,result)
